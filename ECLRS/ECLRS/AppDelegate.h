6c9f4bb6c3aac66349902b3ac350447962b567f3
Maxime <michaelchant@yandex.ru>
2016-10-28 05:23:12 +0200

    int vers
    tss

---
+++ ECLRS/ECLRS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ECLRS
+//
+//  Created by Gold on 10/14/16.
+//  Copyright © 2016 Gold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ECLRS/ECLRS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ECLRS
+//
+//  Created by Gold on 10/14/16.
+//  Copyright © 2016 Gold. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
